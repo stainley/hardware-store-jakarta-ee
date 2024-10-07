b93faf550d578bdf9da8226efcf782ce7e53805a
Stainley Lebron <stainley.lebron@gmail.com>
2024-10-07 11:59:47 -0400

    project structure

---
+++ hardware-store-jpa/src/main/resources/archetype-resources/src/main/java/App.java
@@ -0,0 +1,13 @@
+package $com.hardwarestore;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
